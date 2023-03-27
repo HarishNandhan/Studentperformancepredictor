259cbdc92a2cd1fcbaa772de64c2487b51e55387
HarishNandhan <harishnandhan02@gmail.com>
2023-03-27 11:14:02 +0530

    Modified home.html file

--- application.py
+++ application.py
@@ -38,5 +38,5 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
  
