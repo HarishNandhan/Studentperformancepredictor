a4e5ae3c05a0f3ffd2c20591cfe0134f5606c359
HarishNandhan <84642310+HarishNandhan@users.noreply.github.com>
2023-04-02 13:13:46 +0530

    Path updated

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,8 +9,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path = 'artifact\model.pkl'
-            preprocessor_path = 'artifact\preprocessor.pkl'
+            model_path = os.path.join('artifact','model.pkl')
+            preprocessor_path = os.path.join('artifact','preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
